9a20a3915506bce2e8fe4f1f895ebfb3571ff961
Marina Kravchenko <marishka-k87@yandex.ru>
2022-01-12 22:21:20 +0300

    Update cardreaction.js

--- script/cardreaction.js
+++ script/cardreaction.js
@@ -2,10 +2,6 @@ const cardReaction = document.querySelectorAll(".card__reaction");
 
 for(let i = 0; i < cardReaction.length; i += 1) {
   cardReaction[i].addEventListener("click", function () {
-    if (cardReaction[i].classList.contains("card__reaction_active")) {
-      cardReaction[i].classList.remove("card__reaction_active");
-    } else {
-      cardReaction[i].classList.add("card__reaction_active");
-    }
+    cardReaction[i].classList.toggle("card__reaction_active");
   });
 }
