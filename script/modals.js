1006ae4d7fd6675b4db789f31fb506578e7ca4ff
Marina Kravchenko <marishka-k87@yandex.ru>
2022-03-22 23:06:05 +0300

    +

--- script/modals.js
+++ script/modals.js
@@ -1,17 +1,21 @@
 const closeButtons = document.querySelectorAll(".popup__close");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const popupProfile = document.querySelector("#popup_profile");
+const profileAvatarButton = document.querySelector(".profile__avatar-button");
+const popupAvatar = document.querySelector("#popup_avatar");
 const profileAddButton = document.querySelector(".profile__add-button");
 const popupCard = document.querySelector("#popup_card");
+const profileImage = document.querySelector(".profile__image");
 const profileName = document.querySelector(".profile__name");
 const profileContactInfo = document.querySelector(".profile__contact-info");
 const profileEditForm = document.forms.profile_edit;
 const profileNameEdit = profileEditForm.elements.profile_name;
 const contactInfoEdit = profileEditForm.elements.contact_info;
+const profileAvtarEditForm = document.forms.profile_avatar_edit;
+const profileImageEdit = profileAvtarEditForm.elements.profile_image;
 const addCardForm = document.forms.add_card;
 const placeName = addCardForm.elements.place_name;
 const placeAdres = addCardForm.elements.place_adres;
-
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
@@ -42,6 +46,10 @@ profileEditButton.addEventListener("click", function () {
   openPopup(popupProfile);
 });
 
+profileAvatarButton.addEventListener("click", function () {
+  openPopup(popupAvatar);
+});
+
 profileAddButton.addEventListener("click", function () {
   openPopup(popupCard);
 });
@@ -57,6 +65,12 @@ function submitProfileFormChange(evt) {
   closePopup(popupProfile);
 }
 
+function submitProfileAvatarChange(evt) {
+  evt.preventDefault();
+  profileImage.src = profileImageEdit.value;
+  closePopup(popupAvatar);
+}
+
 function submitFormNewCard(evt) {
   evt.preventDefault();
   cardsContent.prepend(addCard(placeAdres.value, placeName.value));
@@ -65,5 +79,7 @@ function submitFormNewCard(evt) {
 }
 
 profileEditForm.addEventListener("submit", submitProfileFormChange);
+
+profileAvtarEditForm.addEventListener("submit", submitProfileAvatarChange);
 
 addCardForm.addEventListener("submit", submitFormNewCard);
