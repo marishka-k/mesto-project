b710d0aa59fe575d8597b9c6813e778029527496
Oleg Kuzmin <alex-dust@yandex.ru>
2022-04-26 21:38:28 +0300

    +

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,18 @@
 export default class Section {
-  // Первым параметром конструктора принимает объект с двумя свойствами: items и renderer.
-  // Свойство items — это массив данных, которые нужно добавить на страницу при инициализации класса. Вы получаете эти данные от Api.
-  // Свойство renderer — это функция, которая отвечает за создание и отрисовку данных на странице.
-  // Второй параметр конструктора — селектор контейнера, в который нужно добавлять созданные элементы.
   constructor({items, renderer}, selectorContainer) {
-    this._items = items; // получаем с api
-    this._renderer = renderer; // получаем с класса карточек
+    this._items = items;
+    this._renderer = renderer;
     this._selectorContainer = document.querySelector(selectorContainer);
   }
+
   // Содержит публичный метод, который отвечает за отрисовку всех элементов.
-  // Отрисовка каждого отдельного элемента должна осуществляться функцией renderer.
   renderItems() {
     this._items.forEach(item => {
       this._renderer(item);
     });
   }
 
-  // Содержит публичный метод addItem, который принимает DOM-элемент и добавляет его в контейнер.
+  // Содержит публичный метод, который принимает DOM-элемент и добавляет его в контейнер.
   addItem(item, method) {
     if (method === 'append') {
       this._selectorContainer.append(item);
