cf1b276542928e10cc243547eb7563c10c1b9b32
Oleg Kuzmin <alex-dust@yandex.ru>
2022-04-26 20:16:50 +0300

    add popup methods

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,37 +4,44 @@ export default class Popup {
     this._buttonPopupClose = this._popup.querySelector('.popup__close');
   }
 
-   // устанавливает слушателей
+  // открытие попапа
+  open() {
+    this._popup.classList.add('popup_opened');
+    this.setEventListeners();
+  };
+
+  //закрытие попапа
+  close() {
+    this._popup.classList.remove('popup_opened');
+    this.removeEventListeners();
+  };
+
+  // устанавливает слушателей
    setEventListeners() {
     this._buttonPopupClose.addEventListener('click', this._нandleButtonClose);
-    document.addEventListener("keydown", this._нandleEscClose);
+    document.addEventListener('keydown', this._нandleEscClose);
     document.addEventListener('mousedown', this._handleOverlayClose);
-  }
+  };
 
   // удаляет слушателей
   removeEventListeners() {
     this._buttonPopupClose.removeEventListener('click', this._нandleButtonClose);
     document.removeEventListener('keydown', this._нandleEscClose);
     document.removeEventListener('mousedown', this._handleOverlayClose);
-  }
+  };
 
-  // открытие попапа
-  open() {
-    this._popup.classList.add("popup_opened");
-    this.setEventListeners();
-  }
-
-  //закрытие попапа
-  close() {
-    this._popup.classList.remove("popup_opened");
-    this.removeEventListeners();
-  }
-
-
-
-
-
-
-
-
+  // Методы закрытия попапа
+  _нandleButtonClose = () => {
+    this.close();
+  };
+  _нandleEscClose = (evt) => {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  };
+  _handleOverlayClose = (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      this.close();
+    }
+  };
 }
