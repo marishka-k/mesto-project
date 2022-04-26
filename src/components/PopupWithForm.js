d0c84cbacf120eb8c1920fca04c129472b7923da
Oleg Kuzmin <alex-dust@yandex.ru>
2022-04-26 20:26:27 +0300

    add PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,39 @@
+import Popup from "./popup";
+
+export default class PopupWithForm extends Popup {
+  // Кроме селектора попапа принимает в конструктор колбэк сабмита формы. В этом колбэке содержится метод класса Api.
+  constructor({selector, callback}) {
+    super(selector);
+    this._callback = callback;
+    this._form = this._popup.querySelector('.popup__form');
+  }
+
+  // Содержит приватный метод _getInputValues, который собирает данные всех полей формы.
+  _getInputValues() {
+    this._inputList = this._form.querySelectorAll('.popup__item');
+    this._formValues = {};
+    this._inputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+    });
+    return this._formValues;
+  }
+
+  // Перезаписывает родительский метод setEventListeners. Метод setEventListeners класса PopupWithForm должен не только добавлять обработчик клика иконке закрытия, но и добавлять обработчик сабмита формы.
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', this._formSubmit);
+  }
+
+  // Перезаписывает родительский метод close, так как при закрытии попапа форма должна ещё и сбрасываться.
+  close() {
+    super.close();
+    this._form.reset();
+  }
+
+  // Метод submit: передаем данные всех полей формы в callback
+  _formSubmit = (evt) => {
+    evt.preventDefault();
+    this._callback(this._getInputValues());
+    this.close();
+  }
+}
